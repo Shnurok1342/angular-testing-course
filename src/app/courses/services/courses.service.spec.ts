a13f5f259dbcc239628d7117e1efd15ffcc4ad3e
Shnurok1342 <a13bepb@yandex.ru>
2021-03-05 13:47:22 +0300

    (lesson 8-11) test: - HTTP Service testing; - Preventing Unintended HTTP Requests using the HTTP Testing Controller (verify()).

---
+++ src/app/courses/services/courses.service.spec.ts
@@ -0,0 +1,44 @@
+import {CoursesService} from './courses.service';
+import {TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {COURSES} from '../../../../server/db-data';
+
+describe('CoursesService', () => {
+  let coursesService: CoursesService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CoursesService]
+    });
+    coursesService = TestBed.inject(CoursesService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should retrieve all courses', () => {
+    coursesService.findAllCourses().subscribe(courses => {
+      expect(courses).toBeTruthy();
+      expect(courses.length).toBe(12);
+      const course = courses.find(c => c.id === 12);
+      expect(course.titles.description).toBe('Angular Testing Course');
+    });
+    const req = httpTestingController.expectOne('/api/courses');
+    expect(req.request.method).toEqual('GET');
+    req.flush({ payload: Object.values(COURSES) });
+  });
+
+  it('should find a course by id', () => {
+    coursesService.findCourseById(12).subscribe(course => {
+      expect(course).toBeTruthy();
+      expect(course.id).toBe(12);
+    });
+    const req = httpTestingController.expectOne('/api/courses/12');
+    expect(req.request.method).toEqual('GET');
+    req.flush(COURSES[12]);
+  });
+});
