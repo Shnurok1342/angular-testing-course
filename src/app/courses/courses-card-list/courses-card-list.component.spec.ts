f1253a152ce5c6d4408e4792f561cd29a0b2bc25
Shnurok1342 <a13bepb@yandex.ru>
2021-03-05 14:32:55 +0300

    (lesson 15-16) test: - component Testing; - test setup with the waitForAsync test utility.

---
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -0,0 +1,33 @@
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {CoursesCardListComponent} from './courses-card-list.component';
+import {CoursesModule} from '../courses.module';
+
+describe('CoursesCardListComponent', () => {
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule(({
+      imports: [CoursesModule]
+    }))
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+      });
+  }));
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display the course list', () => {
+    pending();
+  });
+
+  it('should display the first course', () => {
+      pending();
+  });
+});
+
+
