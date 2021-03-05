1deaa5ad393d24c0fa9b3bf873f5413d8a44f6a7
Shnurok1342 <a13bepb@yandex.ru>
2021-03-05 14:41:37 +0300

    (lesson 17-18) test: - DOM Interaction; - trigger Change Detection.

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,10 +1,14 @@
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {CoursesCardListComponent} from './courses-card-list.component';
 import {CoursesModule} from '../courses.module';
+import {setupCourses} from '../common/setup-test-data';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('CoursesCardListComponent', () => {
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule(({
@@ -14,6 +18,7 @@ describe('CoursesCardListComponent', () => {
       .then(() => {
         fixture = TestBed.createComponent(CoursesCardListComponent);
         component = fixture.componentInstance;
+        el = fixture.debugElement;
       });
   }));
 
@@ -22,7 +27,11 @@ describe('CoursesCardListComponent', () => {
   });
 
   it('should display the course list', () => {
-    pending();
+    component.courses = setupCourses();
+    fixture.detectChanges();
+    const cards = el.queryAll(By.css('.course-card'));
+    expect(cards).toBeTruthy();
+    expect(cards.length).toBe(12);
   });
 
   it('should display the first course', () => {
